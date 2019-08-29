e089a7003e15b9ccd5683cd1687e3f90d0bd8762
github username <github email address>
2019-08-29 11:15:28 -0400

    Done.

--- app/models/startup.rb
+++ app/models/startup.rb
@@ -1,3 +1,53 @@
 class Startup
+    attr_accessor :name
+    attr_reader :founder, :domain
+    @@all = []
+    def initialize(name,founder,domain)
+        @name = name
+        @founder = founder
+        @domain = domain
+        @@all << self
+    end
 
+    def pivot(domain, name)
+        @domain = domain
+        @name = name
+    end
+
+    def sign_contract(vc,investment,type)
+        FundingRound.new(self,vc,type,investment)
+    end
+
+    def num_funding_rounds
+        FundingRound.all.select{|x| x.startup == self}.length
+    end
+
+    def total_funds
+        sum = 0
+        FundingRound.all.select{|x| x.startup == self}.each{|y| sum+=y.investment}
+        sum
+    end
+
+    def investors
+        arr = []
+        FundingRound.all.select{|x| x.startup == self}.each{|y| arr << y.venture_capitalist}
+        arr.uniq
+    end
+
+    def big_investors
+        investors.select{|x| VentureCapitalist.tres_commas_club.include?(x)}
+    end
+
+    def self.all
+        @@all
+    end
+
+    def self.find_by_founder(f)
+        @@all.find{|x| x.founder == f}
+    end
+
+    def self.domains
+        arr = []
+        @@all.each{|x| arr << x.domain}
+    end
 end
--- app/models/venture_capitalist.rb
+++ app/models/venture_capitalist.rb
@@ -1,3 +1,44 @@
 class VentureCapitalist
+    attr_accessor :name, :total_worth
+    @@all = []
+    def initialize(name, total_worth)
+        @name = name
+        @total_worth = total_worth
+        @@all << self
+    end
+
+    def offer_contract(startup,type,investment)
+        FundingRound.new(startup,self,type,investment)
+    end
+
+    def funding_rounds
+        FundingRound.all.select{|x| x.venture_capitalist == self}
+    end
+
+    def portfolio
+        arr = []
+        funding_rounds.each{|x| arr << x.startup}
+        arr.uniq
+    end
+
+    def biggest_investment
+        arr = []
+        funding_rounds.each{|x| arr << x.investment}
+        arr.max
+    end
+
+    def invested(d)
+        sum = 0
+        funding_rounds.select{|x| x.domain == d}.each{|y| sum+= y.investment}
+        sum
+    end
+
+    def self.all
+        @@all
+    end
+
+    def self.tres_commas_club
+        @@all.select{|x| x.total_worth > 1000000000}
+    end
 
 end
