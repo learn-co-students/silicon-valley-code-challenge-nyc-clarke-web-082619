c20b7407e267e52597640c0fb31b3bd6b9dd3abd
github username <github email address>
2019-08-30 11:39:43 -0400

    done

--- app/models/startup.rb
+++ app/models/startup.rb
@@ -1,3 +1,58 @@
 class Startup
+    @@all = []
 
+    attr_reader  :name, :founder, :domain
+
+    def initialize(name, founder, domain)
+        @name = name
+        @founder = founder
+        @domain = domain
+        @@all << self
+    end
+
+    def pivot(domain, name)
+        @domain = domain
+        @name = name
+    end
+
+    def sign_contract(vc, type, investment)
+        FundingRound.new(vc, self, type, investment)
+    end
+
+    def funding_rounds
+        FundingRound.all.select{|round| round.startup == self}
+    end
+    def num_funding_rounds
+        funding_rounds.length
+    end
+
+    def total_funds
+        funding_rounds.reduce(0) {|sum, round| sum + round.investment}
+    end
+
+    def investors
+        my_investors = []
+        funding_rounds.each do |round|
+            if !my_investors.include?(round.venture_capitalist)
+                my_investors << round.venture_capitalist 
+            end
+        end
+        my_investors
+    end
+
+    # def big_investors
+    #     VentureCapitalist.tres_commas_club.select {|vc| vc}
+    # end
+
+    def self.find_by_founder(founder)
+        all.find{|startup| startup.founder == founder}
+    end
+
+    def self.domains
+        all.map {|startup| startup.domain}
+    end
+
+    def self.all
+        @@all
+    end
 end
--- tools/console.rb
+++ tools/console.rb
@@ -7,5 +7,19 @@ end
 # This is a convenient place to define variables and/or set up new object instances,
 # so they will be available to test and play around with in your console
 
+pied_piper = Startup.new("Pied Piper", "Richard Hendricks", "Compression")
+pied_piper_coin = Startup.new("Pied Piper Coin", "Richard Hendricks", "Cryptocurrency")
+bitcoin = Startup.new("Bitcoin", "Satoshi Nakamoto", "Cryptocurrency")
+
+peter_gregory = VentureCapitalist.new("Peter Gregory", 9999999999.00)
+gavin_belson = VentureCapitalist.new("Gavin Belson", 123456789.1)
+
+round4 = bitcoin.sign_contract(peter_gregory, "Series A", 2666666666)
+round1 = pied_piper.sign_contract(peter_gregory, "Angel", 1000000.21)
+round2 = pied_piper.sign_contract(gavin_belson, "Pre-seed",20000000.4)
+round3 = pied_piper.sign_contract(peter_gregory, "Series A", 55000000.0)
+round5 = pied_piper_coin.sign_contract(peter_gregory, "Series A", 111111.0)
+
+
 binding.pry
 0 #leave this here to ensure binding.pry isn't the last line
