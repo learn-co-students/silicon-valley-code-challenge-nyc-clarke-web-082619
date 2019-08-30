d177b15df221a346ea13f5400630b8459cdd089b
JazzyMussels <jeffmussels@gmail.com>
2019-08-30 11:38:24 -0400

    Done.

--- app/models/startup.rb
+++ app/models/startup.rb
@@ -1,3 +1,55 @@
 class Startup
+  attr_reader :founder, :domain
+  attr_accessor :name
+  @@all = []
+
+  def initialize(name, founder, domain)
+    @name = name 
+    @founder = founder 
+    @domain = domain 
+    @@all << self
+  end
+
+  def pivot(new_name, new_domain)
+    @name = new_name
+    @domain = new_domain
+  end
+
+  def sign_contract(venture_boi, investment, amt)
+    FundingRound.new(self, venture_boi, investment, amt)
+  end
+
+  def funding 
+     FundingRound.all.select{|rounds| rounds.startup == self}
+  end
+
+  def num_funding_rounds
+    funding.length
+  end
+
+  def total_funds
+   funding.map{|rounds| rounds.investment}.reduce(&:+)
+  end
+
+  def investors
+    funding.map{|rounds| rounds.venture_capitalist}.uniq
+  end
+
+  def big_investors
+    investors.select{|investor| VentureCapitalist.tres_commas_club.include?(investor)}.uniq
+  end
+
+  def self.find_by_founder(founder_name)
+    self.all.find{|x| x.founder == founder_name}
+  end
+
+  def self.domains
+    self.all.map{|x| x.domain}
+  end
+
+  def self.all 
+    @@all 
+  end
+
 
 end
--- tools/console.rb
+++ tools/console.rb
@@ -3,9 +3,21 @@ require_relative '../config/environment.rb'
 def reload
   load 'config/environment.rb'
 end
-# Insert code here to run before hitting the binding.pry
-# This is a convenient place to define variables and/or set up new object instances,
-# so they will be available to test and play around with in your console
+startup1 = Startup.new('AppyBoi', 'Jeb', 'Fintech')
+startup2 = Startup.new('Jetfool', 'Lou', 'Transport')
+startup3 = Startup.new('FaceTheft', 'Lisa', 'Social Media')
+venture1 = VentureCapitalist.new('Bill Gates', 50_000_000_000)
+venture2 = VentureCapitalist.new('Warren Buffet', 90_000_000_000)
+venture3 = VentureCapitalist.new('Poor Person', 500_000_000)
+#fund1 = FundingRound.new('AppyBoi', 'Bill Gates', 'Angel', 10_000_000)
+#fund2 = FundingRound.new('Jetfool', 'Poor Person', 'Seed', 20_000_000)
+#fund3 = FundingRound.new('FaceTheft', 'Warren Buffet', 'Series C', 30_000_000)
+
+startup1.sign_contract(venture1, 'Angel', 100_000_000)
+startup1.sign_contract(venture2, 'Seed', 200_000_000)
+startup1.sign_contract(venture3, 'C-Series', 100_000_000)
+startup1.sign_contract(venture1, 'Seed', 200_000_000)
+startup1.sign_contract(venture1, 'C-Series', 300_000_000)
 
 binding.pry
 0 #leave this here to ensure binding.pry isn't the last line
