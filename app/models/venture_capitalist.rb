3fe515733d5813926b42a40875a8cc3b1c9a32af
github username <github email address>
2019-08-31 13:02:29 -0400

    Done

--- app/models/venture_capitalist.rb
+++ app/models/venture_capitalist.rb
@@ -1,3 +1,40 @@
 class VentureCapitalist
+    attr_accessor :name, :total_worth
+
+    @@all = []
+
+    def initialize(name, total_worth)
+        @name = name
+        @total_worth = total_worth
+        @@all << self
+    end
+
+    def self.all
+        @@all
+    end
+
+    def self.tres_commas_club
+        @@all.select {|vc| vc.total_worth > 1000000000}
+    end
+
+    def offer_contract(startup, type, investment)
+        FundingRound.new(startup, self, type, investment)
+    end
+
+    def funding_rounds
+        FundingRound.all.select {|fr| fr.venture_capitalist == self}
+    end
+
+    def portfolio
+        funding_rounds.map {|fr|fr.startup}.uniq {|x|x}
+    end
+
+    def biggest_investment
+        funding_rounds.max {|f1, f2| f1.investment <=> f2.investment}
+    end
+
+    def invested(domain)
+        funding_rounds.select {|fr|fr.startup.domain == domain}.reduce(0) {|start, fr| start + fr.investment}
+    end
 
 end
--- tools/console.rb
+++ tools/console.rb
@@ -3,9 +3,27 @@ require_relative '../config/environment.rb'
 def reload
   load 'config/environment.rb'
 end
-# Insert code here to run before hitting the binding.pry
-# This is a convenient place to define variables and/or set up new object instances,
-# so they will be available to test and play around with in your console
+
+v1 = VentureCapitalist.new("Rory", 200000000000)
+v2 = VentureCapitalist.new("Erin", 45000000000)
+v3 = VentureCapitalist.new("Mikey", 300000000)
+v4 = VentureCapitalist.new("Dave", 15000000)
+v5 = VentureCapitalist.new("Denice", 67000000000)
+
+
+s1 = Startup.new("Uber", "Dave Scar", "Taxis")
+s2 = Startup.new("Facebook", "Tom Leiber", "Social Networking")
+s3 = Startup.new("LinkedIn", "Tina Ruby", "Social Networking")
+
+
+f1 = FundingRound.new(s1, v1, "Angel", 50000)
+f2 = FundingRound.new(s1, v2, "Pre-Seed", 500)
+f3 = FundingRound.new(s2, v3, "Seed", 25000)
+f4 = FundingRound.new(s3, v4, "Series A", 46000)
+f5 = FundingRound.new(s2, v1, "Angel", 3400)
+f6 = FundingRound.new(s3, v2, "Pre-Seed", 1000000)
+f7 = FundingRound.new(s3, v3, "Seed", 1500)
+f8 = FundingRound.new(s1, v3, "Series A", 53000)
 
 binding.pry
 0 #leave this here to ensure binding.pry isn't the last line
